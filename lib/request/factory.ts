dfc3b4479e8ce852437ac1081f739ca8b63cd9c0
totto2727 <h_tsuchida@yumemi.co.jp>
2024-03-18 22:05:50 +0900

    fix: add content type for hono validator

--- lib/request/factory.ts
+++ lib/request/factory.ts
@@ -1,11 +1,17 @@
 // Base URLは何でも良い
 const baseUrl = new URL("https://example.com")
 
-export function createRequest(path: string, body?: unknown) {
+export function createRequest(
+  path: string,
+  body?: Record<string, unknown>,
+): Request {
   return new Request(new URL(path, baseUrl), {
     // CronジョブはGET、QueueのワーカーはPOST
     method: body ? "POST" : "GET",
     // POSTの場合のみbodyを設定
     body: body ? JSON.stringify(body) : undefined,
+    headers: {
+      "content-type": "application/json",
+    },
   })
 }
