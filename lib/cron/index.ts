b6cd201da5757fe83389a8a8cd5735409c82e7d7
totto2727 <h_tsuchida@yumemi.co.jp>
2024-03-18 22:11:52 +0900

    feat: cron helper

--- lib/cron/index.ts
+++ lib/cron/index.ts
@@ -1,12 +1,17 @@
-import { Hono } from "hono/mod.ts"
 import { createRequest } from "@/lib/request/index.ts"
+import type { Hono } from "hono"
 
 export function createCron(hono: Hono, path: string) {
   return async () => {
-    await hono.fetch(createRequest(path))
-  }
+    const result = await hono.fetch(createRequest(path))
 
-  // TODO 例外処理
+    // TODO 例外処理
+    if (!result.ok) {
+      throw new Error(
+        `Failed to cron ${path}: ${result.status} ${result.statusText}`,
+      )
+    }
+  }
 }
 
 export function queuePathToCronName(path: string): string {
