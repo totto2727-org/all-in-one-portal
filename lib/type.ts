8ad575da4c168f22327ba089c1ecd3d70fd7a70a
totto2727 <h_tsuchida@yumemi.co.jp>
2024-03-27 23:03:32 +0900

    feat: normalize string for dates and URLs

--- lib/type.ts
+++ lib/type.ts
@@ -1,1 +1,20 @@
+import { G } from "ts-belt"
+import * as v from "valibot/mod.ts"
+
 export type UnknownRecord = Record<string, unknown>
+
+export const isoDateTimeString = v.transform(
+  v.union([v.string([v.isoTimestamp()]), v.date()]),
+  (i) => {
+    return G.isDate(i) ? i.toISOString() : new Date(i as string).toISOString()
+  },
+)
+
+export const sortedUrlString = v.transform(
+  v.union([v.string([v.url()]), v.instance(URL)]),
+  (i) => {
+    const url = i instanceof URL ? i : new URL(i as string)
+    url.searchParams.sort()
+    return url.toJSON()
+  },
+)
