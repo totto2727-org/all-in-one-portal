ad7188d4bd4f8a793b19261e78c7b7a2f0ca3db8
totto2727 <h_tsuchida@yumemi.co.jp>
2024-04-09 21:45:29 +0900

    refactor: delete unused files

--- app.ts
+++
@@ -1,4 +0,0 @@
-export * as cron from "./app/web-gathering/trigger-cron.ts"
-export * as sourceRss from "./app/web-gathering/source-rss.ts"
-export * as readerRss from "./app/web-gathering/reader-rss.ts"
-// export * as saveKv from "./app/web-gathering/save-kv.ts"
